43432c2e784e0934db4fe0c81873746f40a88e3b
Kurt Van Etten <kurt@vanetten.us>
2020-02-02 14:56:46 -0500

    Complete GET /profiles and GET /profiles/:id endpoints

--- profiles/prof-router.js
+++ profiles/prof-router.js
@@ -11,4 +11,18 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+router.get('/:id', async (req, res, next) => {
+  // get a profile by id
+  try {
+    const profile = await profModel.findById(req.params.id);
+    if (profile) {
+      res.json(profile);
+    } else {
+      res.status(404).json({ message: `Profile ${req.params.id} not found` });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
